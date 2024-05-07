9c8e8ff5818b45aa337d6a5fac8dc9139ff2c3e9
viacheslavk55 <525vkostenko@frhsd.com>
2024-05-07 09:04:52 -0400

    made adjustments to the js

--- script.js
+++ script.js
@@ -23,29 +23,51 @@ function generateTeamCards() {
     teamMembers.forEach(member => {
         const card = document.createElement('div')
         card.classList.add('col-md-4')
-
-
-        //Create a list of skills with <li> tage
-        const skillsList = member.skills.map(skill => `<li> ${skill} </li>`).join('')
-
+    
+        //styling card based on position:
+        let backgroundColor
+    
+        switch (member.position.toLowerCase()) {
+          case 'forward':
+            backgroundColor = '#ffc107' // Yellow for forwards
+            break
+          case 'midfielder':
+            backgroundColor = '#28a745' // Green for midfielders
+            break
+          case 'defender':
+            backgroundColor = '#007bff' // Blue for defenders
+            break
+          case 'goalkeeper':
+            backgroundColor = '#dc3545' // Red for goalkeepers
+            break
+          default:
+            backgroundColor = '#6c757d' // Gray for other positions
+        }
+        card.style.backgroundColor = backgroundColor
+    
+        //Create a list of Skills with <li> tags
+        const skillsList = member.skills
+          .map(skill => `<li> ${skill} </li>`).join('')
+    
         card.innerHTML = `
-            <div class = "card">
-                <div class = "card-header"> ${member.name}</div>
-                <div class = "card-body"
-                    <p><strong>Position:</strong> ${member.position}</p>
-                    <p><strong>Skills:</strong> 
-                    <ul>
-                        ${skillsList}
-                    </ul>
-                    </p>
-                    <p><strong>Strengths:</strong> ${member.strengths}</p>
-                    <p><strong>Weaknesses:</strong> ${member.weaknesses}</p>
-                    <p><strong>Biography:</strong> ${member.biography}</p>
+                <div class = "card">
+                    <div class = "card-header"> ${member.name}</div>
+                    <div class = "card-body">
+                        <p><strong>Position:</strong> ${member.position}</p>
+                        <p><strong>Skills:</strong> 
+                            <ul>
+                                ${skillsList}
+                            </ul>
+                        </p>
+                        <p><strong>Strengths:</strong> ${member.strengths}</p>
+                        <p><strong>Weaknesses:</strong> ${member.weaknesses}</p>
+                        <p><strong>Biography:</strong> ${member.biography}</p>
+                    </div>
                 </div>
-            </div>
-        `
-
+            `
+    
         teamCardsContainer.appendChild(card)
-    })
-}
-window.onload = generateTeamCards()
+      })
+    }
+    
+    window.onload = generateTeamCards()
